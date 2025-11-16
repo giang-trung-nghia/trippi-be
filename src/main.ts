d92443b1b852ff7d49ee45734f29c5c84a72dadf
nghia.gt.ext <nghia.gt.ext@eastgate-software.com>
2025-11-16 10:26:17 +0700

    Add Swagger documentation support and update health check endpoint
    - Added @nestjs/swagger package for API documentation.
    - Updated AppController to include Swagger tags for the health check endpoint.
    - Renamed getHello method to healthCheck in AppService, returning a status message.
    - Configured Swagger in main.ts to serve API documentation at '/docs'.
    - Updated package.json and yarn.lock to include new dependencies.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+  .setTitle('Trippi API')
+  .setDescription('API for Trippi')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  
   await app.listen(process.env.PORT ?? 3000);
+  console.log("Server is running on port http://localhost:" + (process.env.PORT ?? 3000));
+  console.log("Docs are running on port http://localhost:" + (process.env.PORT ?? 3000) + '/docs');
 }
 bootstrap();
