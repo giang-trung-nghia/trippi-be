e845bb177a7a7e19a52a14c092d31becddd00922
giang-trung-nghia <nghia2905.per@gmail.com>
2026-03-14 16:37:06 +0700

    Update database configuration paths to use __dirname for entities and migrations

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -11,8 +11,8 @@ export const dataSourceOptions: DataSourceOptions & TypeOrmModuleOptions = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: ['src/modules/**/entities/*.entity{.ts,.js}'],
-  migrations: ['src/database/migrations/*{.ts,.js}'],
+  entities: [__dirname + '/../modules/**/entities/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
   synchronize: false,
   logging: ['error', 'warn'], // Log only error and warn queries to the console
   namingStrategy: new SnakeNamingStrategy(),
